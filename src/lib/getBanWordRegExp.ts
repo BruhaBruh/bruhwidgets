a679cb7cdd018709339a4b3a65d3eb5d753f37db
BruhaBruh <drugsho.jaker@gmail.com>
2022-12-03 21:04:29 +0500

    feat: improve ban word separator regexp

--- src/lib/getBanWordRegExp.ts
+++ src/lib/getBanWordRegExp.ts
@@ -2,7 +2,7 @@ export const getBanWordRegExp = (banWords: string[]) =>
 	new RegExp(
 		`(${banWords
 			.map((word) => {
-				return word.split('').join('([_\\-\\/\\ |+=.,!]?)+');
+				return word.split('').join('[`~!@#$%^&*()[\\]{}\\\\/+=|:;"\'<>,.?\\-_ ]*');
 			})
 			.join('|')})`,
 		'gi'
